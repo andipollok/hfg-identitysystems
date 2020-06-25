225f6940ee7ee221ca173ea50aa5513bad794e8b
Andreas Koller <mail@andreaskoller.com>
2020-06-25 23:40:16 +0200

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+  let sketch = function(p) {
+    p.setup = function(){
+      p.createCanvas(400, 400);
+      p.background(0);
+      p.fill(255);
+      p.ellipse(p.width/2, p.height/2, p.width/3, p.height/3);
+    }
+  };
+  new p5(sketch, 'container');
